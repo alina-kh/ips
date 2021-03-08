971c6fcdaf2019e15abd9fe00ccc0c3ebedd04d5
Alina Khitrova <61740745+alina-kh@users.noreply.github.com>
2021-03-08 18:31:57 +0300

    Update 4.pas

--- lw2/4/4.pas
+++ lw2/4/4.pas
@@ -1,5 +1,17 @@
-﻿{Реализуйте функцию на языке pascal для получения любого из параметров QUERY_STRING. 
-Она должна принимать строку - название параметра и возвращать строку - значение параметра. Заголовок функции:
+﻿{Реализуйте функцию на языке pascal для получения любого из параметров QUERY_STRING. Она должна принимать строку - название параметра и возвращать строку - значение параметра. Заголовок функции:
+
+	FUNCTION GetQueryStringParameter(Key: STRING): STRING;
+
+	Для проверки этой используйте следующую программу:
+PROGRAM WorkWithQueryString(INPUT, OUTPUT);
+USES
+  DOS;
+BEGIN {WorkWithQueryString}
+  WRITELN(‘First Name: ‘, GetQueryStringParameter(‘first_name’));
+  WRITELN(‘Last Name: ‘, GetQueryStringParameter(‘last_name’));
+  WRITELN(‘Age: ‘, GetQueryStringParameter(‘age’))
+END. {WorkWithQueryString}
+
 }
 PROGRAM HelloUser(INPUT, OUTPUT);
 VAR
