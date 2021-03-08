66f412bdc33415500b801e78a85bf5477434bf9a
Alina Khitrova <61740745+alina-kh@users.noreply.github.com>
2021-03-08 18:28:51 +0300

    Add files via upload

---
+++ lw2/1/1.pas
@@ -0,0 +1,7 @@
+﻿{Напечатайте в HttpResponse значение переменных окружения REQUEST_METHOD, QUERY_STRING, CONTENT_LENGTH, HTTP_USER_AGENT, HTTP_HOST
+
+Чтобы QUERY_STRING не было пустым нужно добавить в url адрес значение, например: ?name=Ivan
+
+CONTENT_LENGTH будет пустым поскольку вы отправляете GET запрос, пока отправить POST запрос не получиться, мы разберем это позже.
+}
+
---
+++ lw2/4/4.pas
@@ -0,0 +1,26 @@
+﻿{Реализуйте функцию на языке pascal для получения любого из параметров QUERY_STRING. 
+Она должна принимать строку - название параметра и возвращать строку - значение параметра. Заголовок функции:
+}
+PROGRAM HelloUser(INPUT, OUTPUT);
+VAR
+firstName: string;
+Var
+lastName: string;
+Var
+age: char;
+BEGIN
+READLN(firstName);
+WRITELN('First Name: ', firstName);
+READLN(lastName);
+WRITELN('Last Name: ', lastName);
+READLN(age);
+WRITELN('Age: ', age);
+END.
+
+
+
+
+
+
+
+
