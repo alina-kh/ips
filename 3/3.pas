3da07eddedd239330ae3183f6e520930d3415c57
Alina Khitrova <61740745+alina-kh@users.noreply.github.com>
2021-03-08 17:47:15 +0300

    Add files via upload

---
+++ 3/3.pas
@@ -0,0 +1,13 @@
+﻿{Реализуйте программу, которая выводит приветствие ‘Hello dear, <name>!’. Имя должно передаваться в параметре name QUERY_STRING, в случае когда параметра name нет, должно выводиться ‘Hello Anonymous!’. В данном задании можно считать что параметр name стоит на первом месте, либо его нет. Можно использовать функции POS COPY для работы со строками
+}
+PROGRAM HelloUser(INPUT, OUTPUT);
+VAR
+Name: STRING;
+BEGIN
+READ(Name);
+IF Name = ''
+THEN
+WRITELN('Hello Anonimus')
+ELSE
+WRITELN('Hello dear, ', Name)
+END.
