b721227bf06f17724a2a341b00e3c315a191d55d
Alina Khitrova <61740745+alina-kh@users.noreply.github.com>
2021-03-15 21:34:18 +0300

    Update index.php

--- lw3/1/index.php
+++ lw3/1/index.php
@@ -1,7 +1,13 @@
 <?php
-// Разработайте приложение Remove Extra Blanks на языке PHP. В запросе GET передается параметр text. Скрипт выводит в стандартный поток вывода этот же текст без пробелов в начале и в конце, между словами один пробел.
+// Разработайте приложение Remove Extra Blanks на языке PHP. В запросе GET передается параметр text. 
+//Скрипт выводит в стандартный поток вывода этот же текст без пробелов в начале и в конце, между словами один пробел.
 
 header("Content-Type: text/plain");
-$text = trim($_GET['text']);
 
-echo str_replace(',' , ' ', $text);
+function removeExtraBlanks()
+{
+    $text = trim($_GET['text']);
+    $res = str_replace(',' , ' ', $text);
+    return $res; 
+}
+echo removeExtraBlanks();
