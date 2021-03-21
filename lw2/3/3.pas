d931b6f6f195914985dd7d9d77d33215f4ef04ec
Alina Khitrova <61740745+alina-kh@users.noreply.github.com>
2021-03-21 21:13:33 +0300

    Update 3.pas

--- lw2/3/3.pas
+++ lw2/3/3.pas
@@ -1,13 +1,20 @@
-﻿{Реализуйте программу, которая выводит приветствие ‘Hello dear, <name>!’. Имя должно передаваться в параметре name QUERY_STRING, в случае когда параметра name нет, должно выводиться ‘Hello Anonymous!’. В данном задании можно считать что параметр name стоит на первом месте, либо его нет. Можно использовать функции POS COPY для работы со строками
-}
 PROGRAM HelloUser(INPUT, OUTPUT);
+USES Dos;
 VAR
-Name: STRING;
+  name: STRING;
 BEGIN
-READ(Name);
-IF Name = ''
-THEN
-WRITELN('Hello Anonimus')
-ELSE
-WRITELN('Hello dear, ', Name)
+  name := GetEnv('QUERY_STRING');
+  IF Name = ''
+    THEN
+    BEGIN
+	  WRITELN('Content-Type: text/plain');
+	  WRITELN;
+      WRITELN('Hello Anonimus');
+	END
+  ELSE
+    BEGIN
+	WRITELN('Content-Type: text/plain');
+    WRITELN;
+    WRITELN('Hello dear, ', name);
+	END;
 END.
