3123ff277022d72545eb98f116f533a32cede72a
Alina Khitrova <61740745+alina-kh@users.noreply.github.com>
2021-03-21 20:38:55 +0300

    Create 1.pas

---
+++ lw3/2/1.pas
@@ -0,0 +1,15 @@
+PROGRAM Print(INPUT, OUTPUT);
+USES Dos;
+BEGIN {Print}
+  WRITELN('Content-Type: text/plain');
+  WRITELN;
+  WRITELN(GetEnv('REQUEST_METHOD'));
+  WRITELN;
+  WRITELN(GetEnv('QUERY_STRING'));
+  WRITELN;
+  WRITELN(GetEnv('CONTENT_LENGTH'));
+  WRITELN;
+  WRITELN(GetEnv('HTTP_USER_AGENT'));
+  WRITELN;
+  WRITELN(GetEnv('HTTP_HOST'));
+END. {Print}
