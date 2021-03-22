272fc22896b2aabca264dcf2e5a02cca18ea4ef6
Alina Khitrova <61740745+alina-kh@users.noreply.github.com>
2021-03-22 21:06:25 +0300

    Update 2.pas

--- lw2/2/2.pas
+++ lw2/2/2.pas
@@ -1,31 +1,31 @@
 PROGRAM SarahRevere(INPUT, OUTPUT);
-USES Dos;
+USES 
+  Dos;
 VAR
   Lanterns: STRING;
-VAR
   B: ShortString;
 BEGIN
   Lanterns := GetEnv('QUERY_STRING');
-  B := copy(Lanterns, 10, 1);
+  B := Copy(Lanterns, 10, 1);
   IF B = '1'
-    THEN
-    BEGIN
-	  WRITELN('Content-Type: text/plain');
-	  WRITELN;
-      WRITELN('Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
-	END
+  THEN
+  BEGIN
+    WRITELN('Content-Type: text/plain');
+    WRITELN;
+    WRITELN('Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
+  END
   ELSE
   IF B = '2'
-    THEN
-    BEGIN
-	  WRITELN('Content-Type: text/plain');
-      WRITELN;
-      WRITELN('Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque');
-    END
+  THEN
+  BEGIN
+    WRITELN('Content-Type: text/plain');
+    WRITELN;
+    WRITELN('Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque');
+  END
   ELSE
-    BEGIN
-	  WRITELN('Content-Type: text/plain');
-      WRITELN;
-      WRITELN('Sarah didnt say');
-    END;	
+  BEGIN
+    WRITELN('Content-Type: text/plain');
+    WRITELN;
+    WRITELN('Sarah didnt say');
+  END;	
 END.
