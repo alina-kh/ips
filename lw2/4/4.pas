22ba09ad2ff74a8573a132f28b1b0a5a2bfaab92
Alina Khitrova <61740745+alina-kh@users.noreply.github.com>
2021-03-25 22:40:16 +0300

    Update 4.pas

--- lw2/4/4.pas
+++ lw2/4/4.pas
@@ -2,22 +2,43 @@ PROGRAM HelloUser(INPUT, OUTPUT);
 USES 
   Dos;
 VAR
-  firstName, lastName, qSrt, age:  string;
-  n: integer;
+  qSrt :  STRING;
+  n, qSrtLength: INTEGER;
+  i,k: INTEGER;
+
+FUNCTION GetQueryStringParameter(key: STRING): STRING;
 BEGIN
   qSrt := GetEnv('QUERY_STRING');
-  Delete(qSrt, 1, 10);
-  n := Pos('lastname=', qSrt);
-  firstName := Copy(qSrt, 1, n-2);
-  Delete(qSrt, 1, 9);
-  Delete(qSrt, 1, n-1);
-  n := Pos('&age=', qSrt);
-  lastName := Copy(qSrt, 1, n-1);
-  n := Pos('&age', qSrt);
-  age := Copy(qSrt,n+5, n);
+  n := Pos(key, qSrt);
+  IF n = 0 
+  THEN 
+  BEGIN
+	GetQueryStringParameter := ''
+  END
+  ELSE 
+  BEGIN
+    qSrtLength := length(qSrt);
+	n := n + length(key) + 1;
+	k := n;
+    FOR i := n TO qSrtLength 
+    DO 
+    BEGIN
+      IF qSrt[i] = '&' 
+      THEN 
+        BREAK;
+      IF i = qSrtLength + 1
+      THEN 
+      BREAK;
+      k := k + 1;
+	END;
+	GetQueryStringParameter := Copy(qSrt, n, k - n);
+  END;
+END;
+
+BEGIN
   WRITELN('Content-Type: text/plain');
   WRITELN;
-  WRITELN('First Name: ', firstName);
-  WRITELN('Last Name: ', lastName);
-  WRITELN('Age: ', age);
+  WRITELN('First Name: ', GetQueryStringParameter('first_name'));
+  WRITELN('Last Name: ', GetQueryStringParameter('last_name'));
+  WRITELN('Age: ', GetQueryStringParameter('age'));
 END.
